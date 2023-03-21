9ed6901d402c06188a0d569003183a7ed339f053
ctoid <funk443@yandex.com>
2023-03-21 20:40:17 +0800

    Some changes to how webpage look, and how org publish files

--- publish.el
+++ publish.el
@@ -7,13 +7,21 @@
 ;; notice and this notice are preserved.  This file is offered as-is,
 ;; without any warranty.
 
-(require 'org)
+(require 'ox-publish)
 
-(setq-local org-publish-project-alist
-            '(("union-songs"
-               :base-directory "~/Documents/git/union-songs/org/"
-               :publishing-directory "~/Documents/git/union-songs/web/"
-               :html-head-extra
-               "<link rel=\"stylesheet\" type=\"text/css\" href=\"../web/content-page.css\">")))
-
-(org-publish "union-songs")
+(let ((org-publish-project-alist
+       '(("union-songs-index"
+          :base-directory "~/Documents/git/union-songs/"
+          :publishing-directory "~/Documents/git/union-songs/"
+          :exclude "README\\.org"
+          :recursive nil)
+         ("union-songs-contents"
+          :base-directory "~/Documents/git/union-songs/org/"
+          :publishing-directory "~/Documents/git/union-songs/web/"
+          :recursive nil
+          :html-head-extra
+          "<link rel=\"stylesheet\" type=\"text/css\" href=\"../web/content-page.css\">")
+         ("union-songs"
+          :components ("union-songs-contents" "union-songs-index"))))
+      (org-html-postamble nil))
+  (org-publish "union-songs" t))
